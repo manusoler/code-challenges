40a4adc0cef45815672db50501bc979018d9f71b
Manuel Soler <manusoler@gmail.com>
2020-09-30 13:39:14 +0200

    Added new sololearn medium challenges

--- sololearn/medium.py
+++ sololearn/medium.py
@@ -3,6 +3,7 @@ import sys
 import math
 import string
 from datetime import datetime
+from ..utils.decorators import challenge
 
 # Challenges
 @challenge(name="Pig Latin", 
@@ -18,6 +19,119 @@ def pig_latin():
         transl += w[1:] + w[0] + "ay "
 
     print(transl[:-1])
+
+@challenge(name="Safety Deposit Boxes", 
+    level="Medium", 
+    desc="""
+        Determine the amount of time it will take you to
+        find the item you are looking for if it takes
+        you 5 minutes to drill into each box"""
+)
+def safety_deposit_boxes():
+    stuff = input().split(',')
+    obj = input()
+    print((stuff.index(obj)+1)*5)
+
+@challenge(name="Snap, Crackle and Pop", 
+    level="Medium", 
+    desc="""
+        Based on the quentities in each bowl, output the noise
+        that they make"""
+)
+def snap_crackle_pop():
+    bowl = []
+    for i in range(6):
+        bowl.append(int(input()))
+    f = lambda x: "Pop" if not x%3 else ("Snap" if not x%2 else "Crackle")
+    print(" ".join([f(s) for s in bowl]))
+
+@challenge(name="Deja Vu", 
+    level="Medium", 
+    desc="""
+        If you are gicen a string of random letters, your task is to
+        evaluate thether any letter is repeated in the string or if 
+        you only hit unique keys while you typing"""
+)
+def deja_vu():
+    string = input()
+    dv = False
+    for i in string:
+        if string.count(i) > 1:
+            dv = True
+            break
+    print("Deja Vu" if dv else "Unique")
+
+@challenge(name="The Spy Life", 
+    level="Medium", 
+    desc="""
+        Create a program that will take the encoded message, flip it 
+        around, remove any characters that are not a letter or a space
+        , and output the hidden message"""
+)
+def the_spy_life():
+    msg = input()
+    print("".join(re.findall(r"[A-Za-z ]",msg)[::-1]))
+
+@challenge(name="Symbols", 
+    level="Medium", 
+    desc="""
+        Take a text that includes some random symbols and translate it 
+        into a text that has none of them. The resulting text should only
+        include letters and numbers"""
+)
+def symbols():
+    print(re.sub(r"[^0-9a-zA-Z ]","",input()))
+
+@challenge(name="Duty Free", 
+    level="Medium", 
+    desc="""
+        Evaluate each item that you purchased to make sure that 
+        didn't spend more than $20 on that particular item. 
+        If you did, you will need to go back to the store to 
+        return it"""
+)
+def duty_free():
+    print('Back to the store' if any([float(x)*1.1 > 20 for x in input().split()]) else 'On to the terminal')
+
+@challenge(name="That's odd...", 
+    level="Medium", 
+    desc="""
+        Find the sum of all even integers in a list of numbers"""
+)
+def thats_odd():
+    size = int(input())
+    sum = 0
+    for i in range(size):
+        num = int(input())
+        sum += num if num % 2 == 0 else 0
+    print(sum)
+
+@challenge(name="No numerals", 
+    level="Medium", 
+    desc="""
+        Take a phrase and replace any instances of an integer 
+        form 0-10 and replace it with the English word that 
+        corresponds to that integer"""
+)
+def no_numerals():
+    p = input()
+    repl = {10:"ten", 0:"zero", 1:"one", 2:"two", 3:"three", 4:"four", 5:"five", 6:"six", 7:"seven", 8:"eight", 9:"nine"}
+    for k,v in repl.items():
+        p = re.sub(r"\b("+str(k)+r")\b", v, p)
+    print(p)
+
+@challenge(name="Secret Message", 
+    level="Medium", 
+    desc="""
+        Create a program that replaces each letter in a message
+        with its corresponding letter in a backwars version of 
+        the English alphabet"""
+)
+def secret_message():
+    alpha = string.ascii_lowercase
+    alp_rev = string.ascii_lowercase[::-1]
+    phrase = input().lower()
+    print("".join((alp_rev[alpha.index(i)] if i in alpha else i) for i in phrase))
 
 @challenge(name="Military Time", 
     level="Medium", 
@@ -42,18 +156,6 @@ def military_time():
     else:
         print("Incorrect time format, must be: HH:MM (AM|PM)")
 
-@challenge(name="That's odd...", 
-    level="Medium", 
-    desc="""
-        Find the sum of all even integers in a list of numbers"""
-)
-def thats_odd():
-    size = int(input())
-    sum = 0
-    for i in range(size):
-        num = int(input())
-        sum += num if num % 2 == 0 else 0
-    print(sum)
 
 @challenge(name="Average Word Length", 
     level="Medium", 
@@ -69,20 +171,6 @@ def average_word_length():
     word_len = len(re.findall(r"[a-zA-Z]", essay))
     print(math.ceil(word_len/words_num))
 
-@challenge(name="No numerals", 
-    level="Medium", 
-    desc="""
-        Take a phrase and replace any instances of an integer 
-        form 0-10 and replace it with the English word that 
-        corresponds to that integer"""
-)
-def no_numerals():
-    p = input()
-    repl = {10:"ten", 0:"zero", 1:"one", 2:"two", 3:"three", 4:"four", 5:"five", 6:"six", 7:"seven", 8:"eight", 9:"nine"}
-    for k,v in repl.items():
-        p = re.sub(r"\b("+str(k)+r")\b", v, p)
-    print(p)
-
 @challenge(name="Youtube link finder", 
     level="Medium", 
     desc="""
@@ -93,56 +181,6 @@ def youtube_link_finder():
     url = input()
     id = url.split('/')[-1]
     print(id if not id.startswith("watch") else id.split('=')[-1])
-
-@challenge(name="Secret Message", 
-    level="Medium", 
-    desc="""
-        Create a program that replaces each letter in a message
-        with its corresponding letter in a backwars version of 
-        the English alphabet"""
-)
-def secret_message():
-    alpha = string.ascii_lowercase
-    alp_rev = string.ascii_lowercase[::-1]
-    phrase = input().lower()
-    print("".join((alp_rev[alpha.index(i)] if i in alpha else i) for i in phrase))
-
-@challenge(name="Symbols", 
-    level="Medium", 
-    desc="""
-        Take a text that includes some random symbols and translate it 
-        into a text that has none of them. The resulting text should only
-        include letters and numbers"""
-)
-def symbols():
-    print(re.sub(r"[^0-9a-zA-Z ]","",input()))
-
-@challenge(name="The Spy Life", 
-    level="Medium", 
-    desc="""
-        Create a program that will take the encoded message, flip it 
-        around, remove any characters that are not a letter or a space
-        , and output the hidden message"""
-)
-def the_spy_life():
-    msg = input()
-    print("".join(re.findall(r"[A-Za-z ]",msg)[::-1]))
-
-@challenge(name="Deja Vu", 
-    level="Medium", 
-    desc="""
-        If you are gicen a string of random letters, your task is to
-        evaluate thether any letter is repeated in the string or if 
-        you only hit unique keys while you typing"""
-)
-def deja_vu():
-    string = input()
-    dv = False
-    for i in string:
-        if string.count(i) > 1:
-            dv = True
-            break
-    print("Deja Vu" if dv else "Unique")
 
 @challenge(name="Converts US date to EU date", 
     level="Medium", 
