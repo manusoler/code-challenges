c1000fdb94669b8261b2232bd7c81b9f46e2687a
Manuel Soler <manusoler@gmail.com>
2020-10-02 19:32:39 +0200

    Fix bug un decorator

--- utils/decorators.py
+++ utils/decorators.py
@@ -5,7 +5,7 @@ Module with some decorators used in the project
 def challenge(*args, **kwargs):
     def inner(func):
         def wrapper(*args1, **kwargs1):
-            print("Running challenge from {}...\nName: {} ({})\nDescription:{}".format(kwargs["website"],kwargs["name"],kwargs["level"],kwargs["desc"]))
+            print("Running challenge from {}...\nName: {} ({})\nDescription:{}".format(kwargs.get("website","?"),kwargs["name"],kwargs["level"],kwargs["desc"]))
             func(*args1, **kwargs1)
         return wrapper
     return inner
